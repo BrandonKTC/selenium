60de87729148d6327d71dc2c49dfaf428bf09bda
Brandon Kwamou <brandonkwamou@gmail.com>
2024-03-06 06:42:14 +0100

    selenium python API

---
+++ Python_API/alerts.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service_obj = Service()
+driver = webdriver.Chrome(service=service_obj)
+
+name = "Brandon"
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.find_element(By.CSS_SELECTOR, "#name").send_keys(name)
+driver.find_element(By.ID, "alertbtn").click()
+time.sleep(2)
+alert = driver.switch_to.alert
+alertText = alert.text
+assert name in alertText
+alert.accept()
